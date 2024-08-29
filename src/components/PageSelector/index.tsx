988f56196617488c88784b5029a8ef359b31efb0
marcos <marcosmafeimachado@gmail.com>
2024-08-29 10:40:08 -0300

    feat: add forward and backward chevrons to PageSelector component

--- src/components/PageSelector/index.tsx
+++ src/components/PageSelector/index.tsx
@@ -1,13 +1,33 @@
 import './styles.css';
+import Chevron from '../../assets/chevron.svg';
+import DoubleChevron from '../../assets/double-chevron.svg';
 
 function PageSelector() {
 	return (
-		<div className="page-selector">
-			<button className="page-selector-button active" disabled>
-				1
-			</button>
-			<button className="page-selector-button">2</button>
-			<button className="page-selector-button">3</button>
+		<div className="page-selector-container">
+			<div className="backward-chevrons">
+				<button className="chevron-button backward-chevron">
+					<img src={DoubleChevron} />
+				</button>
+				<button className="chevron-button backward-chevron">
+					<img src={Chevron} />
+				</button>
+			</div>
+			<div className="page-selector">
+				<button className="page-selector-button active" disabled>
+					1
+				</button>
+				<button className="page-selector-button">2</button>
+				<button className="page-selector-button">3</button>
+			</div>
+			<div className="foward-chevrons">
+				<button className="chevron-button foward-chevron">
+					<img src={Chevron} />
+				</button>
+				<button className="chevron-button foward-chevron">
+					<img src={DoubleChevron} />
+				</button>
+			</div>
 		</div>
 	);
 }
