7f86d0769969cb499e21f735f42b6b319968bfaf
marcos <marcosmafeimachado@gmail.com>
2024-08-30 07:45:04 -0300

    feat: implement search functionality in SearchInput component

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,14 +1,34 @@
 import './styles.css';
 import SearchIcon from '../../assets/search-icon.svg';
+import { useCharacters } from '../../providers';
+import { useEffect, useState } from 'react';
+import { useDebounce } from '../../hooks';
 
 function SearchInput() {
+	const {
+		charactersList: { updateSearchParam },
+	} = useCharacters();
+
+	const [searchVal, setSearchVal] = useState<string | undefined>(undefined);
+
+	const debounceValue = useDebounce(searchVal, 500);
+
+	useEffect(() => {
+		if (debounceValue === undefined) return;
+
+		updateSearchParam(debounceValue);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [debounceValue]);
+
 	return (
 		<div className="input-container">
 			<input
 				type="text"
+				name="search"
 				placeholder="Search"
 				className="search-input"
-				onChange={(e) => console.log(e.target.value)}
+				onChange={(e) => setSearchVal(e.target.value)}
 			/>
 			<img src={SearchIcon} alt="Magnifying glass icon" />
 		</div>
