b1ca95a284dfd5277b43fb4dcdb041ca994dff8b
marcos <marcosmafeimachado@gmail.com>
2024-08-30 16:26:16 -0300

    feat: update ComicCardSkeleton component with animationDelay prop for Skeleton components

--- src/components/ComicCardSkeleton/index.tsx
+++ src/components/ComicCardSkeleton/index.tsx
@@ -6,9 +6,9 @@ function ComicCardSkeleton() {
 		<div className="comic-skeleton">
 			<div>
 				<Skeleton className="comic-title-skeleton" />
-				<Skeleton className="comic-title-skeleton" />
+				<Skeleton className="comic-title-skeleton" animationDelay={200} />
 			</div>
-			<Skeleton className="comic-cover-skeleton" />
+			<Skeleton className="comic-cover-skeleton" animationDelay={400} />
 		</div>
 	);
 }
